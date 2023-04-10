7678b748e9b019796e4b311a138f388d02529454
jcc01 <jcc01@byu.edu>
2023-04-10 09:14:39 -0600

    add nohup

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ npm install
 pip install requests
 
 cp -f patched_webui.py /content/stable-diffusion-webui/extensions/sd-webui-tunnels/scripts/try_cloudflare.py
-node /content/patch/index.js &
+nohup node /content/patch/index.js &
+exit
