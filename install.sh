0e7159b46e76153522ea7213b20390c00d239b28
jcc01 <jcc01@byu.edu>
2023-04-10 00:56:41 -0600

    update install

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+cd /content/patch
+
 npm install
 pip install pika
 
-cp -f ./patched_webui.py /content/stable-diffusion-webui/extensions/sd-webui-tunnels/scripts/try_cloudflare.py
+cp -f patched_webui.py /content/stable-diffusion-webui/extensions/sd-webui-tunnels/scripts/try_cloudflare.py
